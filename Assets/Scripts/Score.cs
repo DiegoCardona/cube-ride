4f984e02a979ace671fd02c5461ba91dc0eafaf0
Diego Cardona <cardona.root@gmail.com>
2020-04-27 00:38:23 -0500

    Add logic to divide score and speed code. Add build

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,26 +5,12 @@ public class Score : MonoBehaviour
 {
     public Text scoreText;
     public Transform player;
-    public Text speedText;
-    private long lastPosition = -45;
-    private long lastTime =  System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
-    private int speed = 0;
-    
     
     // Update is called once per frame
     private void Update()
     {
         float score = (player.position.z + 45) / 10;
         scoreText.text = score.ToString("0");
-        long currentPosition = System.Convert.ToInt64(player.position.z);
-        long currentTime = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
         
-        if (currentPosition > (lastPosition + 10)) {
-            speed = (int)(((currentPosition - lastPosition)*System.Math.Pow(10,3)) /  (currentTime - lastTime));
-            lastPosition = currentPosition;
-            lastTime =  currentTime;
-        }
-
-        speedText.text = speed.ToString();
     }
 }
---
+++ Assets/Scripts/Speed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Speed : MonoBehaviour
+{
+    public Transform player;
+    public Text speedText;
+    private long lastPosition = -45;
+    private long lastTime =  System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    private int speedTxt = 0;
+
+    void Update()
+    {
+        long currentPosition = System.Convert.ToInt64(player.position.z);
+        long currentTime = System.DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        
+        if (currentPosition > (lastPosition + 10)) {
+           
+            speedTxt = (int)(((currentPosition - lastPosition)*System.Math.Pow(10,3)) /  (currentTime - lastTime));
+            Debug.Log(speedTxt);
+            lastPosition = currentPosition;
+            lastTime =  currentTime;
+        }
+
+        speedText.text = speedTxt.ToString("0");
+    }
+}
